f02e21e9c9e0ce216bb2626b1a15d5555b3588ce
Rupesh Pradhan <merupeshpradhan@gmail.com>
2026-08-24 23:51:09 +0530

    refactor: remove unused Link import from SignIn and SignUp components

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 function SignUp({ onClose, onSwitchToSignIn }) {
