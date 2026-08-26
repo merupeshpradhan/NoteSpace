caffcec7b629e419f30b28a29bd1003671d485c2
Rupesh Pradhan <merupeshpradhan@gmail.com>
2026-08-27 00:24:39 +0530

    feat: update NoteLeftHeader component; enhance layout and structure for better note organization

--- client/src/components/NoteLeftHeader.jsx
+++ client/src/components/NoteLeftHeader.jsx
@@ -4,8 +4,13 @@ function NoteLeftHeader() {
   return (
     <section className="h-screen border-green-700 border-r-2 shadow shadow-cyan-500/50">
       <div className="border-b-2 border-dashed p-2 text-center">NoteSpace</div>
-      <div className="h-1/3 border-b-2 grid items-center justify-center">
-        <h3>All note data</h3>
+      <div className="p-2 border-b-2 grid gap-2">
+        <h1 className="border p-1">All Notes :</h1>
+        <div className="grid gap-4">
+          <button>Study Times</button>
+          <button>Marketing Dates</button>
+          <button>Movie Watching Dates</button>
+        </div>
       </div>
       <div className="h-1/2 grid items-center justify-center">
         <h3>User Data</h3>
