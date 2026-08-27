e650f291ef96d31855a0b336caecaf324c8eee84
Rupesh Pradhan <merupeshpradhan@gmail.com>
2026-08-27 20:25:04 +0530

    feat: update NoteLeftHeader and NoteTopHeader components; enhance layout and integrate user profile image

--- client/src/components/Layout/Navbar/NoteLeftHeader.jsx
+++ client/src/components/Layout/Navbar/NoteLeftHeader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function NoteLeftHeader() {
   return (
     <section className="h-screen w-65 fixed left-0 top-0 bg-white border-green-700 border-r-2 shadow shadow-cyan-500/50 z-50">
-      <div className="p-2 text-center">NoteSpace</div>
+      <div className="p-2 mt-3 text-center">NoteSpace</div>
       <div className="h-3/6 p-2 mt-5 border-b-2 grid gap-2">
         <div className="grid gap-4">
           <button>Study Times</button>
--- client/src/components/Layout/Navbar/NoteTopHeader.jsx
+++ client/src/components/Layout/Navbar/NoteTopHeader.jsx
@@ -1,11 +1,20 @@
 function NoteTopHeader() {
   return (
     <section className="fixed top-0 left-65  w-[calc(100%-260px)] border-b-2 p-2 border-dashed bg-white z-10">
-      <div className="grid grid-cols-4 justify-center items-center  ">
-        <div className="text-center">Search note</div>
-        <div className="text-center">Search note</div>
-        <div className="text-center">Create note</div>
-        <div className="text-center">User Detials</div>
+      <div className="w-full grid grid-cols-[auto_auto] justify-between items-center  ">
+        {/* Left Side: Search Bar */}
+        <div>
+          <input placeholder="Search note" className="border py-0.5 px-2" />
+        </div>
+
+        {/* Right side: Profile, Notification, And User Detials in a sub-grid */}
+        <div className="grid grid-flow-col auto-cols-max items-center gap-6">
+          <div className="text-center grid">
+            <img src="user_profile.png" alt="Your detials" width={50} />
+          </div>
+          <div className="">Notification</div>
+          <div className="">User Detials</div>
+        </div>
       </div>
     </section>
   );
