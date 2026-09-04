2aafbb42a4d7c0ab8a545d79fab8693aa5d2569c
Rupesh Pradhan <merupeshpradhan@gmail.com>
2026-09-04 22:34:18 +0530

    feat: implement NoteList component to fetch and display notes with update and delete buttons

--- client/src/components/NotePage/NoteList.jsx
+++ client/src/components/NotePage/NoteList.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import api from "../../Api/api.js";
 
 function NoteList() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    async function fetchNots() {
+      try {
+        const res = await api.get("/note");
+        console.log(res.data.notes);
+        setNotes(res.data.notes || []);
+        console.log(notes);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchNots();
+  }, []);
   return (
     <section className="h-full grid justify-items-center content-center">
-      <p>NoteList</p>
+      <div className="grid grid-cols-2 gap-10">
+        {notes.map((note) => (
+          <div
+            key={note.id}
+            className="w-md grid justify-items-center p-5 border border-cyan-500 rounded-2xl shadow-lg shadow-cyan-400"
+          >
+            <p>{note.id}</p>
+            <p>{note.noteName}</p>
+            <p>{note.description}</p>
+            <div className="flex items-center gap-3 mt-4">
+              <button className="bg-indigo-500 hover:bg-indigo-600 text-white text-sm font-medium px-4 py-1.5 rounded-full shadow-sm transition-colors duration-200 cursor-pointer">
+                Update
+              </button>
+              <button className="bg-red-500 hover:bg-red-600 text-white text-sm font-medium px-4 py-1.5 rounded-full shadow-sm transition-colors duration-200 cursor-pointer">
+                Delete
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
