457720d5462a36f601f28e15980d8a2b27a0f0f5
Rupesh Pradhan <merupeshpradhan@gmail.com>
2026-09-08 22:49:04 +0530

    feat: update maxAge for access token cookie to 1 day for improved security

--- server/src/controller/user.control.js
+++ server/src/controller/user.control.js
@@ -172,7 +172,8 @@ export async function login(req, res) {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
+      // maxAge: 7 * 24 * 60 * 60 * 1000,
+      maxAge: 1 * 24 * 60 * 60 * 1000,
     });
 
     const { password: _, ...userWithoutPassword } = updateUser;
