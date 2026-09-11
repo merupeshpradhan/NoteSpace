6a93680642fa87372f52a37edc702771af60aad1
Rupesh Pradhan <merupeshpradhan@gmail.com>
2026-09-11 21:08:42 +0530

    feat: implement user profile display with data from local storage

--- client/src/components/UserView/Profile.jsx
+++ client/src/components/UserView/Profile.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 function Profile() {
-  return <div>Profile</div>;
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+
+    if (storedUser) {
+      try {
+        setUser(JSON.parse(storedUser));
+      } catch (error) {
+        setUser("");
+      }
+    }
+  }, []);
+
+  return (
+    <section className="w-full h-screen grid justify-items-center items-center">
+      <div className="">
+        <p>
+          <span>Name: </span>
+          {user.name}
+        </p>
+        <p><span>Email: </span>{user.email}</p>
+      </div>
+    </section>
+  );
 }
 
 export default Profile;
